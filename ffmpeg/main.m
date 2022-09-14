9810a2fbe14bf9d79d0eecc438c4cdbaa28c6c73
badwin <yangyudong@linkv.sg>
2022-09-14 16:00:42 +0800

    Initial Commit

---
+++ ffmpeg/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ffmpeg
+//
+//  Created by badwin on 2022/9/14.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
